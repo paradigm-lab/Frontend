0ef6beb2b489f6fa4824649b585e930f4cc4faff
collins <collinsboniface1000@gmail.com>
2021-12-09 15:38:26 +0200

    Exploring more about the Assigment operators and incrementation and decrementation of the integer values

--- operators.js
+++ operators.js
@@ -4,10 +4,16 @@ console.log(2 - 1);
 // Subtraction operator
 var diff = 2 - 7;
 console.log("Differece: " + diff);
+diff++; 
+console.log("After Incrementation: " + diff);
+
 
 // Multiplication operator
 var product = 8 * 2;
 console.log("Product: " + product);
+product--;
+console.log("After Decrementation: " + product);
+
 
 // The division opertor
 var div = 2/2;
@@ -15,3 +21,8 @@ console.log("Division: " + div);
 
 var mod = 5 % 4;
 console.log("Modules: " + mod);
+
+var s = 9;
+s-=8;
+console.log(s);
+
