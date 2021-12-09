54701b2816542868767f0d16ff7cfd9bad944f84
collins <collinsboniface1000@gmail.com>
2021-12-09 15:29:45 +0200

    Exploring more about the javascript operators

---
+++ operators.js
@@ -0,0 +1,17 @@
+// Arithmetic operators 
+console.log(2 - 1);
+
+// Subtraction operator
+var diff = 2 - 7;
+console.log("Differece: " + diff);
+
+// Multiplication operator
+var product = 8 * 2;
+console.log("Product: " + product);
+
+// The division opertor
+var div = 2/2;
+console.log("Division: " + div);
+
+var mod = 5 % 4;
+console.log("Modules: " + mod);
