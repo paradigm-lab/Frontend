63d6955e733fffb33142e3e3c921fcbe5da4744d
collins <collinsboniface1000@gmail.com>
2021-12-09 14:45:02 +0200

    Exploring more about the data types in javascript and the array declaration and assignement

--- dataTypes.js
+++ dataTypes.js
@@ -7,11 +7,27 @@ document.write("Using javascript to print hello in the browser");
 
 var general = 5;
 console.log(typeof general);
+
+
+// The string data type can be initiallized in two ways by using the double and single quotes
+// The string data type 
 general = "Hi";
 console.log(typeof general);
+
+// The string data type 
 general = 'Hi';
 console.log(typeof general);
+
+// Boolean data type
 general = true;
 console.log(typeof general);
+
+// The null data type
 general = null;
 console.log(typeof general);
+
+
+// Array internally in javascript is treated as an object
+general = [5, 6, 7, 8]
+console.log(typeof general);
+console.log(general.length);
