bd75bc6f034e927ab523186ae642114b96debe07
collins <collinsboniface1000@gmail.com>
2021-12-09 14:35:57 +0200

    Declaring a variable and asseing different values in the variable and using the typeof to check the type of the data in the variable like strings, number, boolean and the object

--- dataTypes.js
+++ dataTypes.js
@@ -3,6 +3,15 @@ document.write("Using javascript to print hello in the browser");
 
 // Variable declaration
 // When we declare a variable and we don't assign a value in the variable then that is undefined
-var general;
-console.log(general);
+// undefined is a special values in javascript
 
+var general = 5;
+console.log(typeof general);
+general = "Hi";
+console.log(typeof general);
+general = 'Hi';
+console.log(typeof general);
+general = true;
+console.log(typeof general);
+general = null;
+console.log(typeof general);
