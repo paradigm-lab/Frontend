427b781ee107c3c26f341ff0db4c647144ddd69a
collins <collinsboniface1000@gmail.com>
2021-12-09 15:49:32 +0200

    Exploring more about the conditional operators in javascript

--- operators.js
+++ operators.js
@@ -26,3 +26,23 @@ var s = 9;
 s-=8;
 console.log(s);
 
+
+// Comparison operators
+var s1 = 9;
+var s2 = 8;
+if(s1 == s2){
+	console.log("s1 is equal to s2");
+} else {
+	console.log("s1 is not equal to s2");
+}
+
+
+// The triple = sign checks for the type and 
+// The double = sign checks for the values
+var s1 = 9;
+var s2 = "9";
+if (s1 === s2) {
+	console.log("If part");
+} else {
+	console.log("Else part");
+}
