0ff870f74e5b3095f1efa3af3f720e5df1601b65
collins <collinsboniface1000@gmail.com>
2021-12-09 14:29:35 +0200

    Declaring a variable and when we don't assign a variable it becames undefined

--- dataTypes.js
+++ dataTypes.js
@@ -1,2 +1,8 @@
 console.log("Hello World!!!");
-document.write("Using javascript to print hello in the browser")
+document.write("Using javascript to print hello in the browser");
+
+// Variable declaration
+// When we declare a variable and we don't assign a value in the variable then that is undefined
+var general;
+console.log(general);
+
