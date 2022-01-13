24b203a3efc4a29b8b08539d5cbc6c33a0507c0b
collins <collinsboniface1000@gmail.com>
2022-01-13 19:47:44 +0200

    Explored more about the while loop and the do while loop and show out the execution flow of the program by using the incrementation of the variable

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 // For loop will keep on executing until the condition check return false
 // for(initialization; condition; updation or incremenation and decrementation)
 
+/*
 var subjects = ["Maths", "Physics", "Chemistry"];
 var marks = new Array();
 var num1;
@@ -13,3 +14,25 @@ for (var j = 0; j < subjects.length; j++) {
 for (i in marks){
 	console.log(marks[i]);
 }
+
+*/
+
+
+// While loop will be executed unless the condition statement returns a Boolean false
+// while (condition) "The while loop syntax"
+
+// Conditional loop
+
+var i = 8;
+while (i < 10) {
+	console.log(i);
+	i++;
+}
+
+
+// Exit status loop
+do {
+	console.log(i);
+	i++;
+
+} while(i < 10);
