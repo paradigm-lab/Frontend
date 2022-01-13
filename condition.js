a40d609eef983a35e25cd058724a2e6bb22ae307
collins <collinsboniface1000@gmail.com>
2022-01-13 19:17:01 +0200

    Exploring the switch case in my condition file to prompt the user to enter the his or her age and use the write function to print out the out in the browser

---
+++ condition.js
@@ -0,0 +1,13 @@
+var weight = parseFloat(prompt("Enter your weight: "));
+
+switch(weight){
+	case 1:
+		document.write("Weight is 1");
+		break;
+	case 2: 
+		document.write("Weight is 2");
+		break;
+	case 3:
+		document.write("Weight is 3");
+		break;
+}
