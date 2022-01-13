d97249bbad437ede010398539c040519b3d02f06
collins <collinsboniface1000@gmail.com>
2022-01-13 19:39:12 +0200

    Created an array and use a for loop to insert the marks of each subject in the array and use the for in loop to print the values in the array

--- loops.js
+++ loops.js
@@ -1,4 +1,15 @@
 // For loop will keep on executing until the condition check return false
 // for(initialization; condition; updation or incremenation and decrementation)
 
+var subjects = ["Maths", "Physics", "Chemistry"];
+var marks = new Array();
+var num1;
 
+for (var j = 0; j < subjects.length; j++) {
+	num1 = parseFloat(prompt("Enter marks for subjects " + subjects[j]));
+	marks[j] = num1;
+}
+
+for (i in marks){
+	console.log(marks[i]);
+}
