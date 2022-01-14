c96a4ab70e7280e7d06e195a1deb3fdeefd405c2
collins <collinsboniface1000@gmail.com>
2022-01-14 21:47:02 +0200

    Used the if else if block in the condition file

--- condition.js
+++ condition.js
@@ -2,16 +2,30 @@ var weight = parseFloat(prompt("Enter your weight: "));
 
 switch(weight){
 	case 1:
-		document.write("Weight is 1");
+		document.write("Weight is 1 \n");
 		break;
 	case 2: 
-		document.write("Weight is 2");
+		document.write("Weight is 2 \n");
 		break;
 	case 3:
-		document.write("Weight is 3");
+		document.write("Weight is 3 \n ");
 		break;
 
 	default:
-		document.write("Weight is not in 1, 2, 3");
+		document.write("Weight is not in 1, 2, 3 \n");
 		break;
+}
+
+
+if (weight == 1) {
+	document.write("Weight is 1 \n");
+}
+else if (weight == 2) {
+	document.write("Weight is 2 \n");
+}
+else if (weight == 3) {
+	document.write("Weight is 3 \n");
+}
+else {
+	document.write("Weight is not in 1, 2, 3 \n");
 }
