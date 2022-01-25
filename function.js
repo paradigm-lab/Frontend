c05f59d2e8b211aa71a53a3aea065cdd29c22db5
collins <collinsboniface1000@gmail.com>
2022-01-25 22:09:38 +0200

    Added the comment above the function whereby we have got the Performing task and Calculating a value whereby we had a function that returns a number after the computation of the function

--- function.js
+++ function.js
@@ -1,6 +1,17 @@
+// Performing a task
 function greet(name, lastName) {
 	console.log("Hello " + name + ' ' + lastName);
 }
+
+
+// Calculating a value
+function square(number) {
+	return number * number;
+}
+
+
+let number = square(2);
+console.log(number);
 
 greet("Collins", "Golang");
 greet("Rust", "Smith");
