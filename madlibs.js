fb61dd3570978dddfcbf11ce55a125c29d088036
collins <collinsboniface1000@gmail.com>
2022-01-26 22:56:13 +0200

    Created the variable input for the user to input the verb, noun, Adjective and Adverb

--- madlibs.js
+++ madlibs.js
@@ -1,7 +1,15 @@
+
+var myAdjective = prompt("Adjective: ");
+var myNoun = prompt("Noun: ");
+var myVerb = prompt("Verb: ");
+var myAdverb = prompt("Adverb: ");
+
+
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+
 	// Declaring an empty String
 	var result = "";
-
+	
 	// Concatinate the strings from the parameter in the function
 	result += "The " + myAdjective + myNoun + myVerb + " to the store " + myAdverb;  
 
@@ -10,4 +18,5 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 }
 
 // Calling the function to see the output
-
+var output = wordBlanks(myAdjective, myNoun, myVerb, myAdverb);
+console.log(output);
