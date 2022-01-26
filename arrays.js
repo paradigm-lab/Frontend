eb6c1cb8a909ee4d9713602149cec9a26b130955
collins <collinsboniface1000@gmail.com>
2022-01-26 23:11:09 +0200

    Created a 2D array and used nested for loop to print the array

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 let selectedColors = ['red', 'blue'];
 selectedColors[2] = 1;
-console.log(selectedColors.length);
+//console.log(selectedColors.length);
+
+// Nested Arrays
+var ourArray = [["The universe", 42], ["Everything", 101010]];
+//console.log(ourArray[0][0]);
+
+for(var i = 0; i < 2; i++ ) {
+	for (var j = 0; j < 2; j++) {
+		console.log(ourArray[i][j]);
+	}
+}
