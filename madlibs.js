e941b39d6fa81be02492ace11c6da6d3b7f78375
collins <collinsboniface1000@gmail.com>
2022-01-26 22:59:21 +0200

    Updated the string concatination by giving the space between the words

--- madlibs.js
+++ madlibs.js
@@ -1,8 +1,4 @@
 
-var myAdjective = prompt("Adjective: ");
-var myNoun = prompt("Noun: ");
-var myVerb = prompt("Verb: ");
-var myAdverb = prompt("Adverb: ");
 
 
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
@@ -11,11 +7,16 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 	var result = "";
 	
 	// Concatinate the strings from the parameter in the function
-	result += "The " + myAdjective + myNoun + myVerb + " to the store " + myAdverb;  
+	result += "The " + myAdjective + " " + myNoun + " " +myVerb + " to the store " + myAdverb;  
 
 	// Returning the result to the user
 	return result;
 }
+
+var myAdjective = prompt("Adjective: ");
+var myNoun = prompt("Noun: ");
+var myVerb = prompt("Verb: ");
+var myAdverb = prompt("Adverb: ");
 
 // Calling the function to see the output
 var output = wordBlanks(myAdjective, myNoun, myVerb, myAdverb);
