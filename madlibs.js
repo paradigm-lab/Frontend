e14b9f4c490cea2c8b44911539559a58f32705f9
collins <collinsboniface1000@gmail.com>
2022-01-26 22:47:33 +0200

    Created a function that takes a noun adjective verb and Adverb and returns the result

---
+++ madlibs.js
@@ -0,0 +1,13 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+	// Declaring an empty String
+	var result = "";
+
+	// Concatinate the strings from the parameter in the function
+	result += "The " + myAdjective + myNoun + myVerb + " to the store " + myAdverb;  
+
+	// Returning the result to the user
+	return result;
+}
+
+// Calling the function to see the output
+
