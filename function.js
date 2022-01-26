64121d5616cb76ef9929b5e2339c4f67ed0f1fc2
collins <collinsboniface1000@gmail.com>
2022-01-26 23:59:58 +0200

    Added the comment and Created a function that takes argument and print the output in the console by minize the argments

--- function.js
+++ function.js
@@ -10,6 +10,15 @@ function square(number) {
 }
 
 
+// Passing the Values to functions with arguments
+// Parameter a variable but they act as a placeholder
+function ourFunctionWithArgs(a, b) {
+	console.log(a - b);
+}
+
+ourFunctionWithArgs(2, 2);
+
+
 let number = square(2);
 console.log(number);
 
