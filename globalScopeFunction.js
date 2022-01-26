0d190895eb27f1d7cde1d0c97bf601380b07d78a
collins <collinsboniface1000@gmail.com>
2022-01-27 00:21:29 +0200

    Created a file for the explaining about the scopes in javascript which are the globale and local variable definition

---
+++ globalScopeFunction.js
@@ -0,0 +1,28 @@
+// Scope is referred to the visibillty of the variable
+
+// Declare your variable here
+var myGlobal = 10;
+
+
+function fun1() {
+	// Assign 5 to oppsGlobal here
+	// When we don't declare the variable with var keyword the it becames visible to the function only and not globally
+	// When we don't use the var keyword then it be cames globally 
+	var oopsGlobal = 5;
+}
+
+
+function fun2() {
+	var output = "";
+
+	if (typeof myGlobal != "undefined") {
+		output += " myGlobal: " + myGlobal;
+	}
+	if (typeof oopsGlobal != "undefined") {
+		output += " oopsGlobal: " + oopsGlobal;
+	}
+	console.log(output);
+}
+
+fun1();
+fun2();
