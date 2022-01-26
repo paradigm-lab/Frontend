6a1fc249c91bdd687b63e82141b269776ea94053
collins <collinsboniface1000@gmail.com>
2022-01-26 23:51:14 +0200

    Using a for loop to iterate through my shopping list using nested for loop

--- shoppingList.js
+++ shoppingList.js
@@ -1,2 +1,8 @@
 // Created a shopping list using the 2 Dimenstional Array
 var myList = [["Cereal", 3], ["Milk", 2], ["Bananas", 3], ["Juice", 2], ["Eggs", 4]];
+
+for (var i = 0; i < myList.length; i++) {
+	for(var j = 0; j < 2; j++) {
+		console.log(myList[i][j]);
+	}
+}
