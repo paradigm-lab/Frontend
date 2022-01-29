d8a8a15d009e335b399c5a7d8f11f46b98d43210
collins <collinsboniface1000@gmail.com>
2022-01-29 16:07:34 +0200

    Created a golfCode file that has got the function and a list of the condition in a list

---
+++ golfCode.js
@@ -0,0 +1,9 @@
+var name = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home"];
+
+function golfScore(par, strokes) {
+	
+	return "Change me";
+}	
+
+// Calling the golf score function and passing the argument into the function
+golfScore(5, 4);
