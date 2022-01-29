0d3e304fc910e0f4663c8157f71cae72368b8fac
collins <collinsboniface1000@gmail.com>
2022-01-29 16:42:40 +0200

    Added the debugging to be printed in the console and added the control statement flow

--- golfCode.js
+++ golfCode.js
@@ -1,6 +1,7 @@
 var name = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home"];
 
 function golfScore(par, strokes) {
+
 	if (strokes == 1) {
 		return name[0];
 	} else if (strokes <= par - 2) {
@@ -13,10 +14,13 @@ function golfScore(par, strokes) {
 		return name[4];
 	} else if (strokes == par + 2) {
 		return name[5];
-	} else if (strokes == par + 3) {
+	} else if (strokes >= par + 3) {
 		return name[6];
+	} else {
+		return "Uncountable";
 	}
 }	
 
 // Calling the golf score function and passing the argument into the function
+console.log("Debugging the program");
 console.log(golfScore(5, 4));
