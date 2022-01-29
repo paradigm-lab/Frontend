14796a0d7fe43dd2906cb103a18115e8d7711464
collins <collinsboniface1000@gmail.com>
2022-01-29 16:54:33 +0200

    Updated the html file by adding the link to the javascript file to switchCase

---
+++ switchCase.js
@@ -0,0 +1,25 @@
+function caseInSwitch(val) {
+	var answer = "";
+
+	switch (val) {
+		case 1:
+			answer = "Alpha";
+			break;
+		case 2:
+			answer = "Beta";
+			break;
+		case 3:
+			answer = "Gamma";
+			break;
+		case 4:
+			answer = "Delta";
+			break;
+		default:
+			answer = "Try again";
+			break;
+	}
+
+	return answer;
+}
+
+caseInSwitch(1);
