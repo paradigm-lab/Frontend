f50ff6603c8c6e100a775bd06b0d0ae8865fa30f
collins <collinsboniface1000@gmail.com>
2022-01-29 14:56:35 +0200

    Created a comparison function to check for the argument and the greater and Equallity of the value

--- condition.js
+++ condition.js
@@ -108,4 +108,17 @@ console.log(testGreaterThan(10));
 
 
 
+function testGreaterOrEqual(val) {
+	if (val >= 20) {
+		return "20 or Over";
+	}	
+
+	if (val >= 10) {
+		return "10 or over";
+	}
+
+	return "Less than 10";
+}
+
+console.log(testGreaterOrEqual(10));
 
