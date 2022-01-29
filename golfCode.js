608f4af2c6aec2966bbbbca167f21a9decbe8369
collins <collinsboniface1000@gmail.com>
2022-01-29 16:15:01 +0200

    Created the executional block of the else if block and passed the function into a console

--- golfCode.js
+++ golfCode.js
@@ -1,9 +1,22 @@
 var name = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home"];
 
 function golfScore(par, strokes) {
-	
-	return "Change me";
+	if (strokes == 1) {
+		return name[0];
+	} else if (strokes <= par - 2) {
+		return name[1];
+	} else if (strokes == par - 1) {
+		return name[2];
+	} else if (strokes == par) {
+		return name[3];
+	} else if (strokes == par + 1) {
+		return name[4];
+	} else if (strokes == par + 2) {
+		return name[5];
+	} else if (strokes == par + 3) {
+		return name[6];
+	}
 }	
 
 // Calling the golf score function and passing the argument into the function
-golfScore(5, 4);
+console.log(golfScore(5, 4));
