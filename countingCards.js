a483259bbfbeaf381357b37be39db843d073ea52
collins <collinsboniface1000@gmail.com>
2022-02-06 23:16:49 +0200

    Created a switch case statement block

--- countingCards.js
+++ countingCards.js
@@ -1,6 +1,20 @@
 var count = 0;
 
 function cc(card) {
+	
+	switch (card) {
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count++;
+			break;	
+		case 7:
+		case 8:
+		case 9:
+
+	}
 
 	return "Change Me";
 	
