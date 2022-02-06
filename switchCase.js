914aee8f86a5c42c111d2f3d34c3223a865a7cb0
collins <collinsboniface1000@gmail.com>
2022-02-06 22:39:15 +0200

    Created a new sequentialSizes whereby we don't use the break keyword to break out of the switch case to either return low mid or High

--- switchCase.js
+++ switchCase.js
@@ -22,4 +22,45 @@ function caseInSwitch(val) {
 	return answer;
 }
 
-console.log(caseInSwitch(4));
+
+// Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+	var answer = "";
+
+	switch (val) {
+		case 1:
+
+		case 2:
+
+		case 3:
+
+		case 4:
+			answer = "Low";
+			break;
+		case 5:
+
+		case 6:
+
+		case 7:
+
+		case 8:
+			answer = "Mid";
+			break;
+
+		case 9:
+
+		case 10:
+
+		case 11:
+
+		case 12:
+			answer = "High";
+			break;
+	}
+	console.log("Out of the switch case");
+
+	return answer;
+}	
+
+console.log(sequentialSizes(1));
+// console.log(caseInSwitch(4));
