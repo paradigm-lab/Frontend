194c990685180eba4473a2b08c4e4f7855a54181
collins <collinsboniface1000@gmail.com>
2022-02-06 23:08:42 +0200

    Moved the return statement for the computation in an else block

--- switchCase.js
+++ switchCase.js
@@ -111,15 +111,15 @@ function abTest(a, b) {
 	// Only change code below this line
 	if (a < 0 || b < 0) {
 		return undefined;
+	} else {
+		// Only change code above this line
+		return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 	}
-
-	// Only change code above this line
-	return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
 
 
-console.log(abTest(1, 1));
+console.log(abTest(0, 0));
 // console.log(isLess(3, 3));
 // console.log(chainToSwitch("bob"));
 // console.log(sequentialSizes(5));
