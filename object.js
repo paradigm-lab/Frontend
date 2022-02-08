931c5494f9acfb032bebda5bf863a42660bb2a10
collins <collinsboniface1000@gmail.com>
2022-02-08 21:13:19 +0200

    Created a new object for the Dog with the properties

--- object.js
+++ object.js
@@ -10,3 +10,15 @@ let selection = "name"
 person[selection] = "John"
 
 console.log(person.name);
+
+
+// Objects
+// With the properties
+var myDog = {
+
+	"name": "Quincy",
+	"legs": 3,
+	"tails": 2,
+	"friends":[]
+
+}
