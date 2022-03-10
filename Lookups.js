ee1b6f83fb88f04134888ba7fd0e0f48d0b66f56
collins <collinsboniface1000@gmail.com>
2022-03-10 12:05:52 +0300

    Using a lookup object to check for the values in an object

---
+++ Lookups.js
@@ -0,0 +1,22 @@
+// Setup
+
+function phoneticLookup(val) {
+    var result = "";
+
+
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "frank"
+    };
+
+    result = lookup[val];
+
+    return result;
+
+}
+
+console.log(phoneticLookup("foxtrot"));
