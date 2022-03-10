062581bae7036b5fb6ebdfacd39b079414e95362
collins <collinsboniface1000@gmail.com>
2022-03-10 13:13:47 +0300

    Added the Implementation of the updateRecords function and testing the function

--- recordCollection.js
+++ recordCollection.js
@@ -30,10 +30,23 @@ var collection = {
 };
 
 // Keep a copy of the collection for tests
+// Copy of the object
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 function updateRecords(id, prop, value) {
-
+  if (value === "") {
+    delete collection[id][prop];
+  } else if (prop === "tracks") {
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value);
+  } else {
+    collection[id][prop] = value;
+  }
 
   return collection;
 }
+
+
+// Alter values below to test the function
+updateRecords(2468, "tracks", "test");
+console.log(updateRecords(5439, "artist", "Diamond Platnum"));
