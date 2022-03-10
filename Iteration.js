2f87e8ff53fca5723f964aa9650c9327ab196b10
collins <collinsboniface1000@gmail.com>
2022-03-10 13:48:43 +0300

    Count Backwards, Iterating through an Array using For loop and using a nested for loops

--- Iteration.js
+++ Iteration.js
@@ -33,3 +33,43 @@ for (var i = 1; i < 10; i += 2) {
 }
 
 console.log(youArray);
+
+
+// Count Backwards with a For Loop
+var numArray = [];
+
+for (var i = 9; i > 0; i -= 2) {
+  numArray.push(i);
+}
+
+console.log(numArray);
+
+
+// Iterate Through an Array with a For Loop
+var myArr = [2, 3, 4, 5, 6];
+var total = 0;
+
+for (var i = 0; i < myArr.length; i++) {
+  total = myArr[i] + total;
+}
+
+console.log(total);
+
+
+// Nesting For Loops
+function multiplyAll(arr) {
+  var product = 1;
+
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  
+  return product;
+}
+
+var product = multiplyAll( [ [1,2], [3,4], [5,6,7] ] );
+
+console.log(product);
