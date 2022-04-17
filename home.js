0162c16d409432f4ef08f83c21df3b5e61e9537b
collins <collinsboniface1000@gmail.com>
2022-04-17 19:53:02 +0300

    Explored about numbers in javascript and the opretion that are possiblly done by using javascript such as division, modules, addition, subtracation incrementation and decrementation

--- home.js
+++ home.js
@@ -7,6 +7,29 @@ console.log(b);
 var someNumber = 45;
 console.log(someNumber);
 
+/*
 var age = prompt('What is your age?');
 document.getElementById('someText').innerHTML = "Your age is: " + age;
+*/
+
+
+// Numbers in javascript 
+var num1 = 10;
+
+// Increment num1 by 1
+num1++;
+
+//Decrement num1 by 1
+num1--
+
+console.log(num1); 
+
+// Divide, muliply *, Reminder %
+console.log(num1 % 6);
+
+
+// Increment/decrement by 10
+num1 += 10;
+console.log(num1);
+
 
