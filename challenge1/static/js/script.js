dca2c860dfd9e5e465159dafe27fe6ba961bc7f9
collins <collinsboniface1000@gmail.com>
2022-04-17 21:45:07 +0300

    Added the implementation of the f(x) and Created a DOM and append the element in the flex-box-result

---
+++ challenge1/static/js/script.js
@@ -0,0 +1,11 @@
+// Challenge 1: Your age in Days
+function ageInDays() {
+	var birthYear = prompt("What year were your born?");
+	var ageInDays = (2022 - birthYear) * 365;
+	var h1 = document.createElement('h1');
+	var textAnswer = document.createTextNode('You are' + ' ' + ageInDays + ' ' + 'days old.');
+	h1.setAttribute('id', 'ageInDays');
+	h1.appendChild(textAnswer);
+	document.getElementById('flex-box-result').appendChild(h1);
+}
+
