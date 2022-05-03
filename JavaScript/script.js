e155f8f83477bea26dd011e892a16ea37474fde6
collins <collinsboniface1000@gmail.com>
2022-05-03 09:19:45 +0300

    Created a html file and javascript file which will whereby we have used the inline and external input of the javascript file by Exploring more about variables and Data Types in Javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,32 @@
+// Variable is a container for storing data values.
+
+var person = 'Instructor';
+
+console.log(person);
+
+person = 'Teacher';
+
+console.log(person);
+
+var firstName = 'John', 
+    lastName = 'Doe', 
+    age = 27;
+
+// DataTypes
+// Primitive Data Types -> String, Number, Boolean, Undefined, Null
+// In Javascript everything is an object
+
+var str = 'Javascript is a Programming Language';
+console.log(str);
+
+var num = 27.23;
+console.log(num);
+
+var num1 = 5;
+
+var num2 = 10;
+
+// Null means nothing
+var a = null;
+console.log(a);
+
