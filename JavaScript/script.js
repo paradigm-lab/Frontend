b66b3063454cb3417c92acbf4cfb213ebb801a66
collins <collinsboniface1000@gmail.com>
2022-05-03 10:20:58 +0300

    Explored about JS operators and Comparison Operators

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -26,7 +26,36 @@ var num1 = 5;
 
 var num2 = 10;
 
+var comp = true;
+
 // Null means nothing
 var a = null;
 console.log(a);
+
+// Operators is a special function that take two parameters and return result.
+// Addition, Subtraction, Division and Multiplication
+// It follows the BODMAS (Operator Precedence and Associativity)
+var a = (5 + 5) * 5;
+console.log(a);
+
+// Comparison Operators
+// Comparison operators are used in logical statements to determine equality or difference between variables or values.
+// Double equal compare the value while Triple equal sing compare the value and type
+// != and !== (Strict Not equal) Not Equal 
+// Type corrition
+var a = 5;
+var b = '5';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
