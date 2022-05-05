dcf08a9a68d2fae147b64729d6d44a58108e891f
collins <collinsboniface1000@gmail.com>
2022-05-05 14:58:56 +0300

    Explore more about the function in javascript like function statement and function expression

--- JavaScript/intermediate.js
+++ JavaScript/intermediate.js
@@ -1,3 +1,4 @@
+// Defining Arguments in the function definition
 // Functions let you group a series of statements together to perform a specific task
 function passExam(name, score) {
 	var passUni = 71;
@@ -12,6 +13,41 @@ function passExam(name, score) {
 	}
 }
 
-passExam("John", 75);
+function calcScore(quizScore, assayScore) {
+	var score = quizScore + assayScore;
+	return score;
+}
+
+// Passing Arguments in the function 
+// Calling a function in the function that return Argument that outer function
+passExam("John", calcScore(40, 30));
 passExam("Mary", 65);
 passExam("Bob", 45);
+
+
+// Statements Vs Expressions
+// Statements are instructions to be executed by the web browser and 
+// Expressions produce a value
+
+// Difference between a function statement and expression is that (hoisting)
+// Statement functions can be called before the function definition
+// While Expression function can not be called before the function definition
+
+// Function definition
+function add(a, b) {
+	console.log(a + b);
+}
+
+add(5, 10);
+
+
+// Function Expression
+// Anonymous Function
+var sum = function (a, b) {
+	console.log(a + b);
+}
+
+sum(10, 10);
+
+
+
