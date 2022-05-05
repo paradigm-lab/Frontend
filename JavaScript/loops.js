34750167b4cf10fdbf2eba41918569fb21903e3a
collins <collinsboniface1000@gmail.com>
2022-05-05 18:43:31 +0300

    Explored about the for and while loop by iterating over the list of arrays

---
+++ JavaScript/loops.js
@@ -0,0 +1,32 @@
+// Loops is the process of repeating a block of code when a condition is true
+
+var arr = ['John', 'Nick', 'Bob', 'Michael', 'Mary'];
+
+for (var i = 0; i < arr.length; i++) { 
+	
+	if (arr[i] === "Bob") {
+		continue;
+		console.log(arr[i] + " is my brother");
+	}
+
+	console.log(arr[i]);
+}
+
+/*
+var counter = 0;
+while (counter <= 10) {
+	console.log(counter);
+	counter++;
+}
+*/
+
+console.log();
+
+var colors = ["White", "Red", "Green", "Blue"];
+
+var counter = 0;
+
+while (counter < colors.length) {
+	console.log(colors[counter]);
+	counter++;
+}
