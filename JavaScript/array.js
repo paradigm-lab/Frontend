2b101b07459da01d19a3f81ff99a3cd07791984a
collins <collinsboniface1000@gmail.com>
2022-05-05 15:06:51 +0300

    Created a new file for the arrays to explore more about the array

---
+++ JavaScript/array.js
@@ -0,0 +1,9 @@
+// Array is a special variable, which can hold more than one value at the same time
+// Array's in javascript can consist of multiple data type simulateneous
+
+// Array literal
+// To access the specific item in the array using the index
+var arr = ['John', 'Bob', 'Mary', 10, true, [1, 2, 3]];
+
+console.log(arr[5][0]);
+
