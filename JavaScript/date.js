2a9a1b6e2e423b14af80bd5b1c8fd51a27f9dea5
collins <collinsboniface1000@gmail.com>
2022-05-06 21:33:52 +0300

    Explored more about the date object for the set and get method for the date object

--- JavaScript/date.js
+++ JavaScript/date.js
@@ -1,3 +1,35 @@
 var date = new Date();
 
-console.log(data);
+var year = date.getFullYear();
+
+var month = date.getMonth();
+
+var x = date.setFullYear(2003);
+date.setMonth(20);
+
+console.log(date);
+console.log(year);
+console.log(month);
+
+console.log(x);
+
+// Get Date object methods
+// -> getDate() -> Get the day as a number(1 - 31)
+// -> getDay() -> Get the week day as a number (0 - 6)
+// -> getFullYear() -> Get the four digit year (yyyy)
+// -> getHours() -> Get the hour (0 - 23)
+// -> getMilliseconds() -> Get the milliseconds (0 - 999)
+// -> getMinutes() -> Get the minutes (0 - 59)
+// -> getMonth() -> Get the month (0 - 11)
+// -> getSeconds() -> Get the seconds (0 -59)
+
+// Set Date Object Methods
+// setDate() -> Set the day as a number (1 - 31)
+// setFullYear() -> Set the four digit year (yyyy)
+// setHours() -> Set the hour (0 - 23)
+// setMilliseconds() -> Set the milliseconds (0 - 999)
+// setMinutes() -> Set the minutes (0 - 59)
+// setMonth() -> Set the month (0 - 11)
+// setSeconds() -> Set the seconds (0 - 59)
+
+
