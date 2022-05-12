54807ded28d50027059a4156424b4033cbd70156
collins <collinsboniface1000@gmail.com>
2022-05-12 13:09:43 +0300

    Used the forEach method to iterator over the array of icons and using the tagName to get the HTML collection

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -1,4 +1,4 @@
-var icons = document.getElementsByClassName('fa');
+var icons = document.getElementsByClassName('fa'); // icons is just a HTML collection
 
 for (var i = 0; i < icons.length; i++) {
 	console.log(icons[i]);
@@ -11,3 +11,17 @@ console.log(icons);    // HTMLCollection
 
 iconsArr.push("Hello");
 console.log(iconsArr);
+
+// foreach()
+// foreach() method executes a provided function once for each array element
+// *uses function as a paramenter 
+
+// Converting from HTML collection to an Array collection
+Array.from(icons).forEach(function(icon, index, arr) {
+	console.log(icon, index, arr);	
+});
+
+
+// Using the tag
+var lis = document.getElementsByTagName('li');
+console.log(lis);
