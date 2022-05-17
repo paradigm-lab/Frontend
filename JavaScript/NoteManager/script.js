eac4b1c9a377d94c8c7a8a1607a00ef4112eda01
collins <collinsboniface1000@gmail.com>
2022-05-17 22:57:58 +0300

    Created the new list element and append the child to the list element and the element attribute

--- JavaScript/NoteManager/script.js
+++ JavaScript/NoteManager/script.js
@@ -2,4 +2,27 @@ var ul = document.querySelector("ul");
 
 document.getElementById("add-btn").addEventListener("click", function(e) {
 	e.preventDefault();
+	var addInput = document.getElementById("add-input");
+	var li = document.createElement("li"),
+		firstPar = document.createElement("p"),
+		secondPar = document.createElement("p"),
+		firstIcon = document.createElement("i"),
+		secondIcon = document.createElement("i"),
+		input = document.createElement("input");
+
+	firstIcon.className = "fa fa-pencil-square-o";
+	secondIcon.className = "fa fa-times";
+	input.className = "edit-note";
+	input.setAttribute("type", "text");
+
+	firstPar.textContent = addInput.value;
+
+	secondPar.appendChild(firstIcon);
+	secondPar.appendChild(secondIcon);
+	li.appendChild(firstPar);
+	li.appendChild(secondPar);
+	li.appendChild(input);
+
+	console.log(li);
+
 });
