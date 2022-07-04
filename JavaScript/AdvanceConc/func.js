65ef18efa8e331d0a22d200ec7e1897cb1c55622
collins <collinsboniface1000@gmail.com>
2022-07-04 16:26:38 +0300

    Defined the functions

--- JavaScript/AdvanceConc/func.js
+++ JavaScript/AdvanceConc/func.js
@@ -1,3 +1,4 @@
+/*
 var arr = [1, 2, 3];
 // Adding a new element in the fly
 arr[3] = 4;
@@ -24,3 +25,38 @@ a.obj = {
 a.myFunc = function() {
 	console.log("Hola");
 }
+*/
+
+function sum(num1, num2, fn) {
+	console.log(num1 + num2);
+	console.log(fn());
+}
+
+function done() {
+	return "Function is executed";
+}
+
+sum(5, 10, done);
+
+var scores = [55, 35, 87, 45];
+
+function checkResult(arr, fn) {
+	var newArr = [];
+	for (var i = 0; i < arr.length; i++) {
+		newArr.push(fn(arr[i]));
+	}
+	return newArr;
+}
+
+function passOrFail(score) {
+	return score >= 51;	
+}
+
+function calcDifference(score) {
+	return score - 51;
+}
+
+console.log(checkResult(scores, passOrFail));
+console.log(checkResult(scores, calcDifference));
+
+
