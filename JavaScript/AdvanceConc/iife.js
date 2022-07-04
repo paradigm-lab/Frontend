a906ad499c318e39ba73e9374ed3df772220affc
collins <collinsboniface1000@gmail.com>
2022-07-04 16:55:00 +0300

    Explored about IIFE

--- JavaScript/AdvanceConc/iife.js
+++ JavaScript/AdvanceConc/iife.js
@@ -1,5 +1,35 @@
+// Function Statement
 function a() {
-
+	var c = "From Function a";
+	return c;
 }
 
-a();
+// Function Expression
+var b = function() {
+	var c = "From Function a";
+	return c;
+}
+
+var c = 10;
+var c = 20;
+
+console.log(c);
+
+console.log(a());
+console.log(b());
+
+
+5 + 10;
+
+// Immediately Invoked Function Expression (IIFE)
+(function(name) {
+	console.log("Hello " + name);
+}('John'));
+
+
+var a = function() {
+	console.log("Hi");
+	return 'Hello';
+}();
+
+console.log(a);
